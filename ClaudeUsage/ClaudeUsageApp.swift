35102ff3158605db23ba2774054a86843a4b73ba
Malek Gatoufi <malek.gatoufi@epitech.eu>
2026-04-11 16:21:54 +0200

    Initial Commit

---
+++ ClaudeUsage/ClaudeUsageApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClaudeUsageApp.swift
+//  ClaudeUsage
+//
+//  Created by Malek Gatoufi on 11/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct ClaudeUsageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClaudeUsage/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ClaudeUsage
+//
+//  Created by Malek Gatoufi on 11/04/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
